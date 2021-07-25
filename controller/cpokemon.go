6be9fd49fb08d3994c63135058f730a1b6f7d6dd
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-07-26 01:27:01 +0700

    UPDATE FETCH

--- controller/cpokemon.go
+++ controller/cpokemon.go
@@ -1,11 +1,25 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+type PokemonSpecies struct {
+	Name string `json:"name"`
+}
 
 func GetPokemon(c echo.Context) error {
 	pokemonName := c.FormValue("nama")
@@ -20,6 +34,13 @@ func GetPokemon(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
 	bd, _ := ioutil.ReadAll(res.Body)
+	var responseObject Response
+	json.Unmarshal(bd, &responseObject)
 
-	return c.JSON(http.StatusOK, string(bd))
+	fmt.Println(responseObject.Name)
+	fmt.Println(len(responseObject.Pokemon))
+	for i := 0; i < len(responseObject.Pokemon); i++ {
+		fmt.Println(responseObject.Pokemon[i].Species.Name)
+	}
+	return c.JSON(http.StatusOK, responseObject.Name)
 }
