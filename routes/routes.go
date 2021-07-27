e0b489ffa1e12116f0d5a3c984900ddb1da77236
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-07-28 02:13:22 +0700

    UPDATE RENDER HTML

--- routes/routes.go
+++ routes/routes.go
@@ -3,17 +3,36 @@ package routes
 import (
 	"goecho/controller"
 	"goecho/middleware"
+	"html/template"
+	"io"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func Init() *echo.Echo {
 	app := echo.New()
 	app.Use(middleware.Logger)
-	app.Static("/", "public")
-	// app.GET("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello World")
-	// })
+	t := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("public/*.html")),
+	}
+	app.Renderer = t
+	app.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+			"message": "Golang Web Apps",
+		})
+	})
 	app.GET("/pegawai", controller.FetchAll_mpegawai, middleware.IsAuthenticated)
 	app.POST("/pegawai", controller.Store_mpegawai, middleware.IsAuthenticated)
 	app.PUT("/pegawai", controller.Update_mpegawai, middleware.IsAuthenticated)
